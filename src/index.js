1d5f969273694a8b36318dff8dcd3669932bb7b1
drax3l <alexjeanpierquispesierra@gmail.com>
2024-12-30 18:47:49 -0500

    realizar configuración previa

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import {dirname, join} from 'path'
+import { fileURLToPath } from 'url'
+
+const app= express()
+const __dirname= dirname(fileURLToPath(import.meta.url))
+console.log(join(__dirname, 'views'))
+
+app.set('views', join(__dirname,'views'))
+app.set('view engine', 'ejs')
+
+app.get('/',(req, res)=> res.render('index'))
+
+app.listen(3000)
+console.log('server esta siendo escuchado en el puerto, 3000')
